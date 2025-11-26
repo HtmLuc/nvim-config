085d596d1591e9bbe19509856fc574621eb57af4
Lucas Dantas <lucasvdmedeiros@proton.me>
2025-11-26 09:30:32 -0300

    cleanup: limpeza do código

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,15 +1,13 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        term_colors = true,
-      })
-      vim.cmd.colorscheme "catppuccin"
-    end
-  }
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "mocha",
+      term_colors = true,
+    })
+    vim.cmd.colorscheme "catppuccin"
+  end
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,6 @@ return {
     require('lualine').setup {
       options = {
         theme = 'auto',
-
       }
     }
   end
