cfb5c3033a43961b9a1d4385d8c16ef2c96a248b
Lucas Dantas <lucasvdmedeiros@proton.me>
2025-11-26 22:53:34 -0300

    feat: add mason and lsp plugin

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "ts_ls",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "prismals",
+        "eslint",
+        "biome",
+        "dockerls",
+        "pyright",
+        "ltex",
+      },
+    },
+    dependencies = {
+      {
+        "mason.org/mason.nvim",
+        opts = {
+          ui = {
+            icons = {
+              package_installed = "✓",
+              package_pending = "➜",
+              package_uninstalled = "✗",
+            },
+          },
+        },
+      },
+      "neovim/nvim-lspconfig",
+      config = function()
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      end,
+    },
+  },
+}
