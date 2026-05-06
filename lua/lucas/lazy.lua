60314724cefd6937fbe19f0eada1b1bb62359a8b
Lucas Dantas <lucasvdmedeiros@proton.me>
2026-05-06 14:32:29 -0300

    feat: add more plugins

--- lua/lucas/lazy.lua
+++ lua/lucas/lazy.lua
@@ -14,4 +14,8 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("lucas.plugins")
+require("lazy").setup("lucas.plugins", {
+    change_detection = {
+        notify = false,
+    }
+})
