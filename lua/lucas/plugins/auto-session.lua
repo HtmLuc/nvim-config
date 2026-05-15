6cf420847dd4f69294bc66ad700a4b1a152c2d1f
Lucas Dantas <51703909+HtmLuc@users.noreply.github.com>
2026-05-15 10:54:38 -0300

    fix: fiz sessionRestore deprecated

--- lua/lucas/plugins/auto-session.lua
+++ lua/lucas/plugins/auto-session.lua
@@ -10,7 +10,7 @@ return {
 
     local keymap = vim.keymap
 
-    keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })
+    keymap.set("n", "<leader>wr", "<cmd>AutoSession restore<CR>", { desc = "Restore session for cwd" })
     keymap.set("n", "<leader>ws", "<cmd>AutoSession save<CR>", { desc = "Save session for auto session root dir" })
   end,
 }
