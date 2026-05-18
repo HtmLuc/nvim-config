4f562bae78b5c37a28d375303b301457ed1944c7
Lucas Dantas <51703909+HtmLuc@users.noreply.github.com>
2026-05-18 15:02:57 -0300

    refactor: todo-comments configuration and key mappings

--- lua/lucas/plugins/todo-comments.lua
+++ lua/lucas/plugins/todo-comments.lua
@@ -2,21 +2,26 @@ return {
   "folke/todo-comments.nvim",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local todo_comments = require("todo-comments")
-
-    local keymap = vim.keymap
-    -- TODO: 
-    -- HACK: 
-    -- BUG: 
-    keymap.set("n", "]t", function()
-      todo_comments.jump_next()
-    end, { desc = "Next todo comment" })
-
-    keymap.set("n", "[t", function()
-      todo_comments.jump_prev()
-    end, { desc = "Previous todo comment" })
-
-    todo_comments.setup()
-  end,
+  opts = {},
+  keys = {
+    {
+      "]t",
+      function() require("todo_comments").jump_next() end,
+      desc = "Next todo comment",
+    },
+    {
+      "[t",
+      function() require("todo_comments").jump_prev() end,
+      desc = "Previous todo comment",
+    },
+  },
 }
+
+-- examples:
+-- PERF: 
+-- TODO: 
+-- HACK: 
+-- WARNING: 
+-- BUG: 
+-- FIX: 
+-- NOTE: 
